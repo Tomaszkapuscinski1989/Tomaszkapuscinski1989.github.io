5e31c95a4b7163d43bb195619c0f445486460dc6
Tomaszkapuscinski1989 <136376140+Tomaszkapuscinski1989@users.noreply.github.com>
2023-07-02 18:37:34 +0200

    Add files via upload

---
+++ projekt1/js/slider.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+    centeredSlides: true,
+    effect: 'coverflow',
+ autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+
+});
