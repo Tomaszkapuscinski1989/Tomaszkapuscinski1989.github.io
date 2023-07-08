62244085cf53db31959858aeacdd4375cb486ad1
Tomaszkapuscinski1989 <136376140+Tomaszkapuscinski1989@users.noreply.github.com>
2023-07-08 19:47:11 +0200

    Add files via upload

---
+++ js/tt.js
@@ -0,0 +1,26 @@
+const sun = document.getElementsByClassName('sun')
+const moon = document.getElementsByClassName('moon')
+const body = document.getElementById('body')
+
+let s = localStorage.getItem('s')
+if (s === 'n') {
+    moon[0].classList.add('hiden')
+    body.classList.add('dark')
+} else {
+    sun[0].classList.add('hiden')
+}
+
+moon[0].addEventListener('click', () => {
+    moon[0].classList.add('hiden')
+    sun[0].classList.remove('hiden')
+    localStorage.setItem('s', 'n')
+    body.classList.add('dark')
+})
+
+sun[0].addEventListener('click', () => {
+    sun[0].classList.add('hiden')
+    moon[0].classList.remove('hiden')
+    localStorage.setItem('s', 'd')
+    body.classList.remove('dark')
+})
+
